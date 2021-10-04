8a41ef2d8de67266c7f6f7b1abb22f549e964198
mak1s84 <brabys@gmail.com>
2021-10-04 17:55:24 +0300

    Minor changes.

--- Check.java
+++ Check.java
@@ -6,20 +6,25 @@ public class Check {
         // If there are 4 groups of numbers in the IP address, then do the following.
         if (addressLength.length == 4) {
             // An invalid character (letter or otherwise) can be used as one of the ip address values. An exception is possible...
-            try {
+
                 for (String group : addressLength) {
-                    int groupOfNumber = Integer.parseInt(group);
+                    int groupOfNumber = Integer.MAX_VALUE;
+                    try {
+                    groupOfNumber = Integer.parseInt(group);
+                    } catch (NumberFormatException e) {
+                        System.out.println("An invalid character has been used. -> " + e.getMessage());
+                       checkEverySymbol(group);
+                    }
                     // I check the range of each group of numbers (0 ... 255).
-                    if ((groupOfNumber >= 0) && (groupOfNumber <= 255)) {
-                        System.out.println("This group -> " + groupOfNumber + " <- is within acceptable limits  -  OK.");
-                    } else {
-                        System.out.println("This group -> " + groupOfNumber + " <- is out of range - WRONG.");
+                    if (groupOfNumber != Integer.MAX_VALUE) {
+                        if ((groupOfNumber >= 0) && (groupOfNumber <= 255)) {
+                            System.out.println("This group -> " + groupOfNumber + " <- is within acceptable limits  -  OK.");
+                        } else {
+                            System.out.println("This group -> " + groupOfNumber + " <- is out of range - WRONG.");
+                        }
                     }
                 }
-            } catch (NumberFormatException e) {
-                System.out.println("Error detected -> " + e);
-                e.printStackTrace();
-            }
+
         } else {
             System.out.println("The number of digit groups is not 4.");
         }
@@ -80,12 +85,19 @@ public class Check {
         }
     }
 
-    public boolean isIPV4Address(String address)
-    // There are 4 groups of numbers in ipV4 address. Otherwise, it's something else ...
-    {
-        // Convert the address as a string to an array of strings.
-        String[] arrayAddress = address.split("\\.");
-        return arrayAddress.length == 4;
-    }
+//    public void isIPV4Address(String address)
+//    // There are 4 groups of numbers in ipV4 address. Otherwise, it's something else ...
+//    {
+//        // Convert the address as a string to an array of strings.
+//        String[] arrayAddress = address.split("\\.");
+//        if (arrayAddress.length == 4)
+//        {
+//            System.out.println("It is ip v.4 address.");
+//        }
+//        else
+//        {
+//            System.out.println("Is not ip v.4 address");
+//        }
+//    }
 
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         Check check = new Check();
-        check.checkingGroupOfDigitsForValidRange("192.1J8.4k5.1");
+        check.checkingGroupOfDigitsForValidRange("255.25h5.255.0");
 
 
 
